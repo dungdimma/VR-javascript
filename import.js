bba1f817cf8a7d7b2f0c97a12590e50832c53b1c
dungdimma <dungdimma@gmail.com>
2019-11-14 20:03:16 +0000

    assignment added

---
+++ import.js
@@ -0,0 +1,18 @@
+function display(){
+        document.write("Hello dungdimma in VR class") // my function file name is import.js
+}
+
+// then i will call the function on html as follows
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Hello class of VR in nhub</title>
+    <script type="text/javascript" src="import.js">
+    
+    </script>
+</head>
+<body>
+    <script> display(); </script>
+    
+</body>
+</html>
